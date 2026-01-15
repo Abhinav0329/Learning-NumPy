21e8241cd829c1e8009c7a456dfed63173c2b292
Abhinav <abhinavupadhyay0329@gmail.com>
2026-01-15 13:42:27 +0530

    Add lines to check version

--- array.py
+++ array.py
@@ -2,5 +2,7 @@ import numpy as np
 #Convert the list into a NumPy array
 arr = np.array([1,2,3,4,5])
 print(arr)
-#It shows the data type
+#It shows the Container type
 print(type(arr))
+#Checking the version
+print(f"Numpy Version:{np.__version__}")
